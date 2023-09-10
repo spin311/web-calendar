9595d6c895b3dc6a4772e1de7dae2fc219234bfd
spin311 <svit.spindler@gmail.com>
2023-09-10 13:42:41 +0200

    added script

--- run.sh
+++ run.sh
@@ -1,2 +1,66 @@
-tsc
-node ./dist/server/server.js
+#!/bin/bash
+#TODO: add comments, remove tsc command from final version
+port=3001
+
+
+function checkConnection {
+    if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null ; then
+        return 1
+    fi
+    return 0
+    
+}
+
+while true; do
+  echo "Enter a command ( r=run in background, f=run in foreground, s=stop server, x=exit):"
+  read input
+
+  case $input in
+    r)
+      checkConnection
+        if [ $? -eq 1 ]; then
+            echo "Process already running on port $port.\n Press 1 if you want to kill the process and 0 to continue"
+            read decision
+            if [ $decision -eq 1 ]; then
+                kill $(lsof -t -i :$port)
+            else
+                continue
+            fi
+        else
+            echo "Running server in background..."
+            tsc
+            node ./dist/server/server.js &
+        fi
+      
+      ;;
+    f)
+ checkConnection
+    if [ $? -eq 1 ]; then
+        echo "Process already running on port $port.\n Press 1 if you want to kill the process and 0 to continue"
+        read decision
+        if [ $decision -eq 1 ]; then
+            kill $(lsof -t -i :$port)
+        else
+            continue
+        fi
+    else
+        echo "Running server in foreground..."
+        tsc
+        node ./dist/server/server.js 
+    fi
+      
+      ;;
+    s)
+      echo "Stopping server..."
+      kill $(lsof -t -i :$port)
+      ;;
+    x)
+      echo "Exiting..."
+      break
+      ;;
+    *)
+      echo "Invalid command."
+      ;;
+  esac
+done
+
