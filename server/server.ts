8a12fb749933ec427b14b08a8b860d90b40fedcb
spin311 <svit.spindler@gmail.com>
2023-09-17 13:17:07 +0200

    server reformat and comments

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import * as path from 'path';
-
+//create a new express application instance
 const app = express();
 const port = 3001;
 
+// Configure Express to serve static files from the root directory
 app.use(express.static(
-    // get the absolute path to root directory
-    path.join(__dirname, "../.."))); 
+  // get the absolute path to root directory
+  path.join(__dirname, "../..")));
 
-app.get("/", (req: Request, res: Response)=> {
-    res.sendFile(path.join(__dirname, "../../calendar.html"));
+// Define a route for the root URL ("/") and send the calendar.html file
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../calendar.html"));
 });
 
 app.listen(port, () => {
